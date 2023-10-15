3964c5722b9bd0b7e8ca450af170d542315ad214
AV <anatoliyvilemson@gmail.com>
2023-10-15 19:39:12 +0500

    Add files via upload

---
+++ Rle/Test/ChekingCompression.cs
@@ -0,0 +1,18 @@
+namespace Test
+{
+    public class Tests
+    {
+        [Test]
+        public void ChekingCompression()
+        {
+            var chek = new Rle();
+            var input = "DDDDHHHHkHK";
+            var example = chek.Compression(input);
+
+            Assert.That(example, Is.Not.EqualTo("4D,5H,1k,1K"));
+            Assert.That(example, Is.Not.EqualTo("4D,4H,2K,1H"));
+            Assert.That(example, Is.EqualTo("4D,4H,1k,1H,1K"));
+
+        }
+    }
+}
